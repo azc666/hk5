8b3d9c90b8450248a9d908627073264afab132b0
Allen Cohen <azc666@gmail.com>
2018-01-09 13:39:33 -0500

    mail typo

--- app/Mail/OrderProductionEmail.php
+++ app/Mail/OrderProductionEmail.php
@@ -33,6 +33,6 @@ class OrderProductionEmail extends Mailable
     {
         return $this->view('emails.orderProduction')
             ->subject('HK Order Portal Production Email')
-            ->from('support@g-d.com.com');
+            ->from('support@g-d.com');
     }
 }
